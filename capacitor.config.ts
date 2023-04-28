95e4814b5e9af3c83883096078ab736ef645589d
folsze <81622754+folsze@users.noreply.github.com>
2023-04-28 08:56:58 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-fixed-position-top-right-element-disappears-after-tilt-on-ios',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
